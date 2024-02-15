f915edab4feac985f11cf7460e4d4ee127c06949
smart <smart@smhrd>
2024-02-15 12:00:21 +0900

    수원

--- EPA/src/main/java/com/epa/controller/viewsController.java
+++ EPA/src/main/java/com/epa/controller/viewsController.java
@@ -1,20 +1,11 @@
 package com.epa.controller;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.epa.entity.Member;
-import com.epa.mapper.MemberMapper;
+
 
 @Controller
 public class viewsController {
-
-	@Autowired
-	private MemberMapper mapper;
 
 
 	// 마이페이지 이동 경로
